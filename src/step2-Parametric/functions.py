47698f2b5ad24bf7b0addbb099ed6b7c9c1d342f
javierjg001 <javierjg001@gmail.com>
2025-12-14 18:01:39 +0100

    parametric examples finished

--- src/step2-Parametric/functions.py
+++ src/step2-Parametric/functions.py
@@ -28,7 +28,11 @@ def create_circle(center_x, center_y, radius, layer=1, datatype=0, num_points=64
 def create_polygon(points, layer=1, datatype=0):
     return gdspy.Polygon(points, layer=layer, datatype=datatype)
 
-# Offset example: outline of the rectangle
+# ==========================================================
+# FUNCTIONS PARAMETRIC TRANSFORMATIONS
+# ==========================================================
+
+# --------------------OFFSET FUNCTION-------------------------
 def offset_shape(shape, distance, layer=1, datatype=0):
     return gdspy.offset(
         shape,
@@ -37,10 +41,6 @@ def offset_shape(shape, distance, layer=1, datatype=0):
         layer=layer,
         datatype=datatype
     )
-
-# ==========================================================
-# FUNCTIONS PARAMETRIC TRANSFORMATIONS
-# ==========================================================
 
 # --------------------ROTATE FUNCTION-------------------------
 def rotate_shape(shape, angle, center=None):
@@ -53,3 +53,18 @@ def rotate_shape(shape, angle, center=None):
     shape.rotate(angle, center)
     return shape
 
+# --------------------SCALE FUNCTION-------------------------
+def scale_shape(shape, scalex, scaley=None, center=None):
+    if center is None: #if no center is provided, use the shape's bounding box center
+        bb = shape.get_bounding_box()
+        center = (
+            (bb[0][0] + bb[1][0]) / 2,
+            (bb[0][1] + bb[1][1]) / 2
+        )
+    shape.scale(scalex, scaley, center)
+    return shape
+
+# --------------------TRANSLATE FUNCTION-------------------------
+def translate_shape(shape, dx, dy):
+    shape.translate(dx, dy)
+    return shape
--- src/step2-Parametric/main.py
+++ src/step2-Parametric/main.py
@@ -1,6 +1,6 @@
 import gdspy
 import numpy as np
-from functions import create_rectangle
+from functions import *
 
 # ==========================================================
 # MAIN PARAMETRIC SHAPES
@@ -13,11 +13,19 @@ lib = gdspy.GdsLibrary()
 top = lib.new_cell("TOP_PARAMETRIC")
 
 # add rectangle using the function
-top.add(create_rectangle(0, 0, 10, 10, layer=10, datatype=0))
+top.add(create_rectangle(0, 0, 10, 20, layer=0, datatype=0))
+top.add(create_circle(10, 10, 5, layer=10, datatype=0))
+top.add(create_polygon([(20, 0), (25, 10), (30, 0), (35,10)], layer=50, datatype=0))
+
+top.add(offset_shape(create_rectangle(40, -30, 10, 20), distance=10, layer=10, datatype=0))
+top.add(rotate_shape(create_rectangle(40, 0, 10, 10, layer=20, datatype=0), angle=np.pi/4))
+top.add(scale_shape(create_polygon([(20, -20), (25, -10), (30, -20), (35,-10)], layer=60, datatype=0), scalex=3, scaley=0.5))
+top.add(translate_shape(create_rectangle(50, 0, 10, 10, layer=30, datatype=0), dx=5, dy=5))
+
 
 # Write GDS file
 try:
-    lib.write_gds("gds/parametric_shapes.gds", cells=[top])
+    lib.write_gds("gds/parametric_shapes.gds")
     print("parametric_shapes.gds generated successfully.")
 except Exception as e:
     print("Error generating parametric_shapes.gds:", e)
